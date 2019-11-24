300aec188fdb928d742b744cc72b2a58dc0eb06d
Saheed Akinbile <sakin070@uottawa.ca>
2019-11-24 16:45:42 -0500

    Food info

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -12,7 +12,15 @@ import {
 import React from 'react';
 
 
-const items = [{image:'assets/food.jpg', date: '2019-04-12', ingredients: 'a picture of a cat' , calories:'100KCal'}];
+const items = [
+    {image:'assets/food.jpg', date: '2019-04-12', ingredients: 'Lettuce, Carrot, Onions, Peas, Tomatoes, Avocado' , calories:'33KCal'},
+    {image:'assets/food2.jpg', date: '2019-04-12', ingredients: 'Bacon, Eggs, Potatoes, Mushroom, Asparagus, Tomatoes' , calories:'100KCal'},
+    {image:'assets/food3.jpg', date: '2019-04-12', ingredients: 'Cucumber, Lettuce, Tomatoes' , calories:'1KCal'},
+    {image:'assets/food4.jpg', date: '2019-04-12', ingredients: 'Pasta, Mushroom, Green Beans, Shrimp, Tomatoes' , calories:'22Cal'},
+    {image:'assets/food5.jpg', date: '2019-04-12', ingredients: 'Cheese, Corn, Beef, Pita Bread' , calories:'31KCal'},
+    {image:'assets/food6.jpg', date: '2019-04-12', ingredients: 'Waffles, Orange, Strawberries ' , calories:'40KCal'},
+    {image:'assets/food7.jpg', date: '2019-04-12', ingredients: 'Beef' , calories:'15KCal'},
+];
 
 const Home: React.FC = () => {
   return (
