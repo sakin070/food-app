6e308c40a23234a9098ef8779b774f1cd94165e8
Saheed Akinbile <sakin070@uottawa.ca>
2019-11-24 16:47:40 -0500

    build data

---
+++ build/static/js/5.265bf684.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpfood-app"]=this["webpackJsonpfood-app"]||[]).push([[5],{179:function(n,t,o){"use strict";o.r(t),o.d(t,"startStatusTap",(function(){return i}));var e=o(24),i=(o(3),function(){var n=window;n.addEventListener("statusTap",(function(){Object(e.g)((function(){var t=n.innerWidth,o=n.innerHeight,i=document.elementFromPoint(t/2,o/2);if(i){var c=i.closest("ion-content");c&&c.componentOnReady().then((function(){Object(e.m)((function(){return c.scrollToTop(300)}))}))}}))}))})}}]);
+//# sourceMappingURL=5.265bf684.chunk.js.map
